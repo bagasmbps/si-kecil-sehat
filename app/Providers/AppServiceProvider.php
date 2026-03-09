77ad710aa656f431715b62f6185d7d69182366e3
bagasmbps <bgasprima@gmail.com>
2026-03-09 19:41:08 +0700

    fix https production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,14 +7,20 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        if (app()->environment('local')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
