49022ec0ab0ee00504786db828e5f78313a9d26b
bagasmbps <bgasprima@gmail.com>
2026-03-09 20:07:10 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,20 +7,14 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (app()->environment('local')) {
             URL::forceScheme('https');
         }
     }
